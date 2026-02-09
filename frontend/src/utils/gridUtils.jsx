3be16cb132f069757ed67c5efc8c2c1953d66f8a
ploy <ployw@unr.edu>
2026-02-08 20:29:17 -0800

    udpate empty date display

--- frontend/src/utils/gridUtils.jsx
+++ frontend/src/utils/gridUtils.jsx
@@ -150,7 +150,7 @@ export const concatNameValueGetter = (firstName, lastName) => {
 };
 
 export const dateTimeFormatter = (params) => {
-  if (!params.value) return "-";
+  if (!params.value) return "---";
   return dayjs(params.value).format("MM/DD/YYYY, h:mm A");
 };
 
